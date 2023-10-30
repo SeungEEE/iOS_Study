b892f44d84b772e21c2ce9d7d50ea4153d0ae6c2
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-10-30 15:09:46 +0900

    StargramClone - Finish

--- MyApp/StargramClone/StargramClone/ReelsViewController.swift
+++ MyApp/StargramClone/StargramClone/ReelsViewController.swift
@@ -13,7 +13,9 @@ class ReelsViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private let videoURLStrArr = ["dummyVideo", "dummyVideo02"]
+    private var nowPage = 0
+    
+    private let videoURLStrArr = ["video01", "video02", "dummyVideo01", "dummyVideo02"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +30,33 @@ class ReelsViewController: UIViewController {
     private func setupcollectionView() {
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.decelerationRate = .fast // 스크롤이 빨리 되도록
+        collectionView.register(
+            ReelsCell.self,
+            forCellWithReuseIdentifier: ReelsCell.identifier)
         
-        collectionView.register(ReelsCell.self, forCellWithReuseIdentifier: ReelsCell.identifier)
+        startLoop()
+    }
+    
+    private func startLoop() {
+        let _ = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+            self.moveNextPage()
+        }
+    }
+    
+    private func moveNextPage() {
+        let itemCount = collectionView.numberOfItems(inSection: 0)
+        
+        nowPage += 1
+        if (nowPage >= itemCount) {
+            // 마지막 페이지
+            nowPage = 0
+        }
+        
+        collectionView.scrollToItem(
+            at: IndexPath(item: nowPage, section: 0),
+            at: .centeredVertically,
+            animated: true)
     }
 }
 
@@ -45,6 +72,12 @@ extension ReelsViewController: UICollectionViewDelegate, UICollectionViewDataSou
             for: indexPath) as? ReelsCell else { return UICollectionViewCell() }
         cell.setupURL(videoURLStrArr.randomElement()!)
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) as? ReelsCell {
+            cell.videoView?.cleanUP()
+        }
     }
 }
 

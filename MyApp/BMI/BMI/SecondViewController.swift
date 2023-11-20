b0e451c5a9309ea05fd2a77817cbe40299afd03d
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-11-20 23:15:18 +0900

    BMI 계산기

---
+++ MyApp/BMI/BMI/SecondViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SecondViewController.swift
+//  BMI
+//
+//  Created by 이승진 on 2023/11/20.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var bmiNumberLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    
+    var bmiNumber: Double?
+    var adviceString: String?
+    var bmiColor: UIColor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       makeUI()
+    }
+    
+    func makeUI() {
+        
+        bmiNumberLabel.clipsToBounds = true
+        bmiNumberLabel.layer.cornerRadius = 8
+        bmiNumberLabel.backgroundColor = .gray
+        
+        backButton.clipsToBounds = true
+        backButton.layer.cornerRadius = 5
+        backButton.setTitle("다시 계산하기", for: .normal)
+        
+        guard let bmi = bmiNumber else { return }
+        bmiNumberLabel.text = String(bmi)
+        
+        adviceLabel.text = adviceString
+        
+        bmiNumberLabel.backgroundColor = bmiColor
+
+        
+    }
+    
+    
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}

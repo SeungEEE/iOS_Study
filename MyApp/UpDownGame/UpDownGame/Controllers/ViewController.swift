d2d712bc9f1efb41d4dfe21c0519b91871ca4033
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-12-03 15:53:39 +0900

    UpDownGame - MVC 패턴 적용

--- MyApp/UpDownGame/UpDownGame/Controllers/ViewController.swift
+++ MyApp/UpDownGame/UpDownGame/Controllers/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     // 컴퓨터가 랜덤으로 숫자 선택(1...10)
     var comNumber = Int.random(in: 1...10)
     
+    var upDownManager = UpDownManager()
+    
     // 내가 선택한 숫자를 담는 변수
 //    var myNumber: Int = 1
     
@@ -22,15 +24,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        reset()
+    }
+    
+    func reset() {
         // 1) 메인 레이블에 "선택하세요"
         mainLabel.text = "선택하세요"
         
         // 2) 숫자 레이블은 "" (빈 문자열)
         numberLabel.text = ""
+        upDownManager.resetNum()
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
-        
         // 1) 버튼의 숫자를 가져와야함
         guard let numString = sender.currentTitle else { return }
     
@@ -40,35 +46,23 @@ class ViewController: UIViewController {
         // 3) 선택한 숫자를 변수에대가 저장 (선택사항) 문자열 -> 숫자 변환한 후 저장
 //        guard let num = Int(numString) else { return }
 //        myNumber = num
+        
+        guard let num = Int(numString) else { return }
+        upDownManager.setUsersNum(num: num)
     }
     
     @IBAction func selectButtonTapped(_ sender: UIButton) {
-        
         // 또 다른 방법 제시
         // 숫자 레이블에 있는 문자열 가져오기(옵셔널 벗기기)
         guard let myNumString = numberLabel.text else { return }
         // 타입 변환 (문자열 -> 정수)
         guard let myNumber = Int(myNumString) else { return }
         
-        // 컴퓨터의 숫자와 내가 선택한 숫자 비교 UP / Down / Bingo (메인 레이블)
-        if comNumber > myNumber {
-            mainLabel.text = "Up!"
-        } else if comNumber < myNumber {
-            mainLabel.text = "Down!"
-        } else {
-            mainLabel.text = "Bingo😄"
-        }
+        upDownManager.setUsersNum(num: myNumber)
+        mainLabel.text = upDownManager.getUpDownResult()
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
-        
-        // 1) 메인 레이블이 "선택하세요"
-        mainLabel.text = "선택하세요"
-        
-        // 2) 컴퓨터의 랜덤 숫자를 다시 선택하게
-        comNumber = Int.random(in: 1...10)
-        
-        // 3) 숫자 레이블을 "" (빈 문자열)
-        numberLabel.text = ""
+        reset()
     }
 }
---
+++ MyApp/UpDownGame/UpDownGame/Models/UpDownManager.swift
@@ -0,0 +1,36 @@
+//
+//  UpDownManager.swift
+//  UpDownGame
+//
+//  Created by 이승진 on 2023/12/03.
+//
+
+import Foundation
+
+struct UpDownManager {
+    // 컴퓨터가 랜덤으로 숫자 선택 (1...10)
+    private var comNumber = Int.random(in: 1...10)
+    
+    // 내가 선택한 숫자를 담는 변수
+    private var myNumber: Int = 1
+    
+    mutating func resetNum() {
+        comNumber = Int.random(in: 1...10)
+        myNumber = 1
+    }
+    
+    mutating func setUsersNum(num: Int) {
+        myNumber = num
+    }
+    
+    func getUpDownResult() -> String {
+        // 컴퓨터와 숫자와 내가 선택한 숫자를 비교 Up / Down / Bingo 리턴
+        if comNumber > myNumber {
+            return "Up"
+        } else if comNumber < myNumber {
+            return "Down"
+        } else {
+            return "Bingo😎"
+        }
+    }
+}

7ccefbfb86655e2af36edff54e2f2eeb3861b27a
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-12-20 23:05:05 +0900

    N&T - N&TProject
    스토리보드

---
+++ MyApp/N&T/N&TProject/N&TProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  N&TProject
+//
+//  Created by 이승진 on 2023/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

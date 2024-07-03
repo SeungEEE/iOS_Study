57428f259f42967be08939ca864078ab6524cb7b
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-07-03 23:47:40 +0900

    SwiftUI(Tutorials) - Landmarks

---
+++ SwiftUI/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 이승진 on 2024/07/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                } //: HStack
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            } //: VStack
+            .padding()
+            
+            Spacer()
+        } //: VStack
+    }
+}
+
+#Preview {
+    ContentView()
+}

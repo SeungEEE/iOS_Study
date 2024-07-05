6bec05c89d0e27cdbcc5918d6df227b48a3895d9
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-07-05 19:49:24 +0900

    Restart-App

---
+++ SwiftUI/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by 이승진 on 2024/07/04.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

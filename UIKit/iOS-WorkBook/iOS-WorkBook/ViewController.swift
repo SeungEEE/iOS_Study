49de12446e5c57ba3e538c4f3636115797e81576
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-12-09 17:47:16 +0900

    [Chore] StoryBoard 삭제 및 초기설정

---
+++ UIKit/iOS-WorkBook/iOS-WorkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-WorkBook
+//
+//  Created by 이승진 on 12/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

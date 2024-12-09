7d3d2b658b281660b83f83ec2d9d0598c96b6bb1
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2024-12-09 21:55:29 +0900

    Week 1: 변수와 상수, 기본 자료형, String과 String 보간법, 타입 선언 방법

---
+++ Swift/Swift-WorkBook.playground/Pages/Week01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+// MARK: - Week 1: 변수와 상수, 기본 자료형, String과 String 보간법, 타입 선언 방법
+
+import Foundation
+
+/// 여기에 답의 코드를 작성해주시기 바랍니다.
+/// 조건 1 : 이름은 상수로, 나이는 변수로 지정해주세요
+/// 조건 2 : 문자열 보간법을 사용해서 출력해주시기 바랍니다.
+//let name: String = "UMC"
+//var age: Int = 10
+//
+//print("이름: \(name)")
+//print("나이: \(age)")
+
+
+/// ___ 로 표시되어 있는 부분을 채워 넣어주시기 바랍니다.
+/// 조건 1 : 자료형을 넣어주세요
+/// 조건 2 : 타입 변환을 해주세요
+/// 조건 3 : 보간법을 사용해주세요
+
+//// let numA: ___ = 10
+//let numA: Int = 10
+//let numB: Double = 10.2
+//
+//// let sum = numA + _____
+//let sum = numA + Int(numB)
+//
+//// print(______)
+//print("결과 : \(sum)")
+
+// 여기에 답의 코드를 작성해주시기 바랍니다.
+let name: String = "Jeong"
+let age: Int = 28
+let message = "이름 : \(name), 나이 : \(age)"
+
+print(message)

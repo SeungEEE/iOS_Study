17295f5f4d60cb9f81beb312ba3eca7aef4f15cc
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2025-01-05 22:01:03 +0900

    Week 6: 실습문제

---
+++ Swift/Swift-WorkBook.playground/Pages/Week06.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+// MARK: - Week 6: 프로퍼티
+
+
+import Foundation
+
+// MARK: - 프로퍼티 실습
+// 1. "사각형의 너비: \(rectangle.width) cm, 높이: \(rectangle.height) cm"와 같이 출력되도록 작성해주세요!
+
+// 2. "사각형의 면적: \(rectangle.area) cm²"와 같이 출력되도록 작성해주세요!
+
+// 3. "사각형의 면적: \(rectangle.area) \(Rectangle.unit)²" 형식으로 출력되도록 작성해주세요!
+

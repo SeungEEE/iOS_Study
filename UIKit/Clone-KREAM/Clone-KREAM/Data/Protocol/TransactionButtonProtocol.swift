60244c5600aa2458158f8844a75ac6f2b309e6a8
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2025-01-08 15:06:29 +0900

    [Feat] TransactionButton 커스텀 추가

---
+++ UIKit/Clone-KREAM/Clone-KREAM/Data/Protocol/TransactionButtonProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionButtonProtocol.swift
+//  Clone-KREAM
+//
+//  Created by 이승진 on 1/8/25.
+//
+
+import UIKit
+
+protocol TransactionButtonProtocol {
+    var priceLabel: UILabel { get set }
+    var subLabel: UILabel { get set }
+}

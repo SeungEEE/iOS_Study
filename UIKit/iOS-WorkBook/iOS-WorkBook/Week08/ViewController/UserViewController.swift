b413e8d3cff19a3745351b99454f647e467665ef
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2025-02-10 18:17:39 +0900

    [Feat] postFile 추가

--- UIKit/iOS-WorkBook/iOS-WorkBook/Week08/ViewController/UserViewController.swift
+++ UIKit/iOS-WorkBook/iOS-WorkBook/Week08/ViewController/UserViewController.swift
@@ -16,6 +16,9 @@ class UserViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+        
+        getUserInfo()
+        postFile()
     }
     
     private func getUserInfo() {
@@ -25,6 +28,26 @@ class UserViewController: UIViewController {
                 do {
                     let userResponse = try response.map([UserResponseModel].self)
                     print("Successfully mapped response: \(userResponse)")
+                } catch {
+                    print("Mapping error: \(error.localizedDescription)")
+                }
+            case .failure(let error):
+                print("Network request error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func postFile() {
+        guard let image = Image(named: "week8Image")?.pngData() else {
+            fatalError("Error: Invalid image!")
+        }
+        
+        provider.request(.postFile(image: image)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let fileResponse = try response.map(FileResponseModel.self)
+                    print("Successfully mapped respons: \(fileResponse)")
                 } catch {
                     print("Mapping error: \(error.localizedDescription)")
                 }

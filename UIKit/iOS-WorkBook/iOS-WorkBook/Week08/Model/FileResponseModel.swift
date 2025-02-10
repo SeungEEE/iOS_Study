23840e9f6fc7330df13b42660c5033533f1dcebd
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2025-02-10 18:17:24 +0900

    [Feat] FileResponseModel 추가

---
+++ UIKit/iOS-WorkBook/iOS-WorkBook/Week08/Model/FileResponseModel.swift
@@ -0,0 +1,14 @@
+//
+//  FileResponseModel.swift
+//  iOS-WorkBook
+//
+//  Created by 이승진 on 2/10/25.
+//
+
+import Foundation
+
+struct FileResponseModel: Codable {
+    let originalname: String
+    let filename: String
+    let location: String
+}

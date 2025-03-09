b5520b93aa0503761ced13f1824c944920369f2e
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2025-03-09 17:42:10 +0900

    [Feat] Add a watchOS target

---
+++ SwiftUI/Landmarks/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by 이승진 on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

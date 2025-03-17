b81fda54517a4d974825c3e5dd4fb0f61c4beb52
Seungjin Lee <sjin3687@gmail.com>
2025-03-17 12:42:33 +0900

    [Chore] Font 및 Color 스타일 지정

---
+++ SwiftUI/SwiftUI-Assignment/SwiftUI-Assignment/SwiftUI-Assignment/Resources/Fonts/Fonts.swift
@@ -0,0 +1,63 @@
+//
+//  Fonts.swift
+//  SwiftUI-Assignment
+//
+//  Created by 이승진 on 3/17/25.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    static var mainTextExtraBold24: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.extraBold.font(size: 24)
+    }
+    
+    static var mainTextBold20: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.bold.font(size: 20)
+    }
+    
+    static var mainTextSemiBold18: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.semiBold.font(size: 18)
+    }
+    
+    static var mainTextMedium16: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.medium.font(size: 16)
+    }
+    
+    static var mainTextRegular13: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.regular.font(size: 13)
+    }
+    
+    static var mainTextRegular09: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.regular.font(size: 9)
+    }
+    
+    static var mainTextRegular18: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.regular.font(size: 18)
+    }
+    
+    static var mainTextRegular12: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.regular.font(size: 12)
+    }
+    
+    static var mainTextBold24: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.bold.font(size: 24)
+    }
+    
+    static var mainTextSemiBold24: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.semiBold.font(size: 24)
+    }
+    
+    static var mainTextSemiBold16: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.semiBold.font(size: 16)
+    }
+    
+    static var mainTextLight14: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.light.font(size: 14)
+    }
+    
+    static var makeMedium18: UIFont {
+        return SwiftUIAssignmentFontFamily.Pretendard.medium.font(size: 18)
+    }
+}

08f8c10aa973269d6b4ed441bb87c11a52b068de
Seungjin Lee <sjin3687@gmail.com>
2025-03-17 12:58:17 +0900

    [Chore] 프로젝트 파일 생성

---
+++ SwiftUI/Clone-Starbucks/Clone-Starbucks/Clone_StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clone_StarbucksApp.swift
+//  Clone-Starbucks
+//
+//  Created by 이승진 on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Clone_StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

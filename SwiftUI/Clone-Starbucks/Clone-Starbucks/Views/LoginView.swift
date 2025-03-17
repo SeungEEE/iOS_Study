1246b31c2b506a4391c36ebe0b66988b32e5f5d3
Seungjin Lee <sjin3687@gmail.com>
2025-03-17 13:01:10 +0900

    [Chore] 폴더 및 파일 생성

---
+++ SwiftUI/Clone-Starbucks/Clone-Starbucks/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Clone-Starbucks
+//
+//  Created by 이승진 on 3/17/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}

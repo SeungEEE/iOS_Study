96f0092d24cb2043af2fc9f5a1ce11412e9e752a
Seungjin Lee <sjin3687@gmail.com>
2025-03-18 10:19:38 +0900

    [Feat] Splash 화면 구현

--- SwiftUI/Clone-Starbucks/Clone-Starbucks/Views/SplashView.swift
+++ SwiftUI/Clone-Starbucks/Clone-Starbucks/Views/SplashView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.green01)
+            
+            Image("logo")
+                .frame(width: 168, height: 168)
+        }
+        .ignoresSafeArea()
     }
 }
 

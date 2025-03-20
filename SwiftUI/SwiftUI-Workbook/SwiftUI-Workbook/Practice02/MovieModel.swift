a0ca123230e049041a03667a02d6800d8ce1945b
Seungjin Lee <sjin3687@gmail.com>
2025-03-20 11:42:47 +0900

    [Chore] 폴더 구조 작성

---
+++ SwiftUI/SwiftUI-Workbook/SwiftUI-Workbook/Practice02/MovieModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieModel.swift
+//  SwiftUI-Workbook
+//
+//  Created by 이승진 on 3/20/25.
+//
+
+import Foundation
---
+++ SwiftUI/SwiftUI-Workbook/SwiftUI-Workbook/Practice02/MovieView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieView.swift
+//  SwiftUI-Workbook
+//
+//  Created by 이승진 on 3/20/25.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieView()
+}

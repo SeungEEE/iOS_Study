fd0a32396fd8584ea21f1943c4d29d39ba83e158
Seungjin Lee <sjin3687@gmail.com>
2025-03-21 13:03:41 +0900

    [Feat] MovieModel 작성

--- SwiftUI/SwiftUI-Workbook/SwiftUI-Workbook/Practice02/MovieModel.swift
+++ SwiftUI/SwiftUI-Workbook/SwiftUI-Workbook/Practice02/MovieModel.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct MovieModel {
+    let movieImage: Image // 영화 포스터 이미지
+    let movieName: String // 영화 이름
+    let movieLike: Int // 영화 좋아요 갯수
+    let movieReserCount: Double // 영화 예매율
+}

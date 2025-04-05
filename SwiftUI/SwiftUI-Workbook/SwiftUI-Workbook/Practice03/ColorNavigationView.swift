071845862412a9222ae5c4d13f2d88026c5447d6
Seungjin Lee <sjin3687@gmail.com>
2025-04-05 20:14:13 +0900

    [Feat] ColorNavigationView 작성

--- SwiftUI/SwiftUI-Workbook/SwiftUI-Workbook/Practice03/ColorNavigationView.swift
+++ SwiftUI/SwiftUI-Workbook/SwiftUI-Workbook/Practice03/ColorNavigationView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct ColorNavigationView: View {
+    @Bindable var viewModel: RainbowViewModel
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 70) {
+            if let selectedRainbowModel = viewModel.selectedRainbowModel {
+                Text("현재 선택된 색상 \n\(selectedRainbowModel.returnColorName())")
+                    .font(.title)
+                    .foregroundStyle(.black)
+                
+                Button {
+                    viewModel.appleLogoColor = selectedRainbowModel.returnColor()
+                    dismiss()
+                } label: {
+                    Text("사과 색 바꾸기")
+                        .padding(.vertical, 43)
+                        .padding(.horizontal, 36)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 30)
+                                .fill(.clear)
+                                .stroke(.black, style: .init(lineWidth: 1))
+                        }
+                }
+            }
+        }
+        .navigationTitle("색 네비")
     }
-}
-
-#Preview {
-    ColorNavigationView()
 }

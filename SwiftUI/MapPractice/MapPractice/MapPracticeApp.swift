3c141f9026f22e71ed21e6447b04c96e0c517b9b
Seungjin Lee <sjin3687@gmail.com>
2025-04-28 19:21:16 +0900

    [Chore] Info & MapPracticeApp 수정

---
+++ SwiftUI/MapPractice/MapPractice/MapPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPracticeApp.swift
+//  MapPractice
+//
+//  Created by 이승진 on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MapPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HybridMapContainerView()
+        }
+    }
+}

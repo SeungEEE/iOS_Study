e414faa45990b7168a8e24213d8399a0b53bbbdb
Seungjin Lee <sjin3687@gmail.com>
2025-08-05 16:52:54 +0900

    [Feat] UIConstants 구현

---
+++ SwiftUI/Clone-Starbucks/Clone-Starbucks/Resources/UIConstants/UIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  UIConstants.swift
+//  Clone-Starbucks
+//
+//  Created by 이승진 on 8/5/25.
+//
+
+import Foundation
+
+enum UIConstants {
+    static let defaultHorizontalPadding: CGFloat = 19
+    static let defaultBottomPadding: CGFloat = 40
+    static let shopHorizontalPadding: CGFloat = 16
+    static let defaultScrollBottomPadding: CGFloat = 120
+    static let scrollBottomPadding: CGFloat = 10
+}

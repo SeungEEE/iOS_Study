1de78ca3751a8375df484f0aa559b22c8502b9a3
Seungjin Lee <sjin3687@gmail.com>
2025-08-11 22:44:11 +0900

    [Feat] 디저트 더미 데이터 DessertDataSource 구현

---
+++ SwiftUI/Clone-Starbucks/Clone-Starbucks/Models/Domain/Home/Dessert/DessertDataSource.swift
@@ -0,0 +1,18 @@
+//
+//  DessertDataSource.swift
+//  Clone-Starbucks
+//
+//  Created by 이승진 on 8/5/25.
+//
+
+import Foundation
+
+struct DessertDataSource {
+    static let dessertItems: [DessertMenuItem] = [
+        .init(name: "너티 크루아상", thumbnailImage: .dessert01),
+        .init(name: "매콤 소시지 불고기", thumbnailImage: .dessert02),
+        .init(name: "미니 리프 파이", thumbnailImage: .dessert03),
+        .init(name: "뺑 오 쇼콜라", thumbnailImage: .dessert04),
+        .init(name: "소시지&올리브 파이", thumbnailImage: .dessert05)
+    ]
+}

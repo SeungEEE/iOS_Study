4cf08018863797aa489a284758aec5d95b0f48d0
Seungjin Lee <sjin3687@gmail.com>
2025-08-11 22:39:48 +0900

    [Add] 자주 사용하는 폰트 추가

--- SwiftUI/Clone-Starbucks/Clone-Starbucks/Extension/Font+.swift
+++ SwiftUI/Clone-Starbucks/Clone-Starbucks/Extension/Font+.swift
@@ -48,52 +48,77 @@ extension Font {
         return .custom(type.value, size: size)
     }
     
+    /// ExtraBold
     static var mainTextExtraBold24: Font {
         return .pretend(type: .extraBold, size: 24)
+    }
+    
+    /// Bold
+    static var mainTextBold24: Font {
+        return .pretend(type: .bold, size: 24)
     }
     
     static var mainTextBold20: Font {
         return .pretend(type: .bold, size: 20)
     }
     
-    static var mainTextSemiBold18: Font {
-        return .pretend(type: .semiBold, size: 18)
-    }
     
-    static var mainTextMedium16: Font {
-        return .pretend(type: .medium, size: 16)
-    }
+    /// SemiBold
     
-    static var mainTextMedium18: Font {
-        return .pretend(type: .medium, size: 18)
-    }
-    
-    static var mainTextRegular13: Font {
-        return .pretend(type: .regular, size: 13)
-    }
-    
-    static var mainTextRegular09: Font {
-        return .pretend(type: .regular, size: 9)
-    }
-    
-    static var mainTextRegular18: Font {
-        return .pretend(type: .regular, size: 18)
-    }
-    
-    static var mainTextRegular12: Font {
-        return .pretend(type: .regular, size: 12)
-    }
-    
-    static var mainTextBold24: Font {
-        return .pretend(type: .bold, size: 24)
+    static var mainTextSemiBold38: Font {
+        return .pretend(type: .semiBold, size: 38)
     }
     
     static var mainTextSemiBold24: Font {
         return .pretend(type: .semiBold, size: 24)
     }
     
+    static var mainTextSemiBold18: Font {
+        return .pretend(type: .semiBold, size: 18)
+    }
+    
     static var mainTextSemiBold16: Font {
         return .pretend(type: .semiBold, size: 16)
+    }
+    
+    static var mainTextSemiBold14: Font {
+        return .pretend(type: .semiBold, size: 14)
+    }
+    
+    static var mainTextSemiBold13: Font {
+        return .pretend(type: .semiBold, size: 13)
+    }
+    
+    /// Medium
+    static var mainTextMedium18: Font {
+        return .pretend(type: .medium, size: 18)
+    }
+    
+    static var mainTextMedium16: Font {
+        return .pretend(type: .medium, size: 16)
+    }
+    
+    
+    /// Regular
+    static var mainTextRegular18: Font {
+        return .pretend(type: .regular, size: 18)
+    }
+    
+    static var mainTextRegular13: Font {
+        return .pretend(type: .regular, size: 13)
+    }
+    
+    static var mainTextRegular12: Font {
+        return .pretend(type: .regular, size: 12)
+    }
+    
+    static var mainTextRegular09: Font {
+        return .pretend(type: .regular, size: 9)
+    }
+    
+    /// Light
+    static var mainTextLight24: Font {
+        return .pretend(type: .light, size: 24)
     }
     
     static var mainTextLight14: Font {

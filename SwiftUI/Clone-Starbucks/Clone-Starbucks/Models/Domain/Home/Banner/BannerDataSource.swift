25512c24e32804ee23505977f22d1d77b51e36a0
Seungjin Lee <sjin3687@gmail.com>
2025-08-11 22:41:03 +0900

    [Feat] 홈 배너 아이템 모델 BannerDataSource 구현

---
+++ SwiftUI/Clone-Starbucks/Clone-Starbucks/Models/Domain/Home/Banner/BannerDataSource.swift
@@ -0,0 +1,22 @@
+//
+//  BannerDataSource.swift
+//  Clone-Starbucks
+//
+//  Created by 이승진 on 8/5/25.
+//
+
+import Foundation
+
+struct BannerDataSource {
+    static let bannerItems: [BannerItem] = [
+        .init(image: .banner01),
+        .init(image: .banner02),
+        .init(image: .banner03),
+        .init(image: .banner04),
+        .init(image: .banner05),
+        .init(image: .banner06),
+        .init(image: .banner07),
+        .init(image: .banner08),
+        .init(image: .banner09)
+    ]
+}

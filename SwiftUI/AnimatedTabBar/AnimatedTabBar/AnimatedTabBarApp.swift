016edaa66978db01a36f1022837362c8f5212838
Seungjin Lee <sjin3687@gmail.com>
2026-01-20 23:41:47 +0900

    [SwiftUI] AnimatedTabBar(1)

---
+++ SwiftUI/AnimatedTabBar/AnimatedTabBar/AnimatedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTabBarApp.swift
+//  AnimatedTabBar
+//
+//  Created by 이승진 on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
